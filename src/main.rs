dfbc5c982ac5544816d3e632be403a5824d07c6e
Mayur Satardekar <mayursatardekar10@gmail.com>
2025-08-15 09:42:25 +0530

    Some more string practice

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,46 @@
 fn main() {
-    let s = String::from("hello");
+    // let s = String::from("hello");
 
-    take_ownership(s);
+    // take_ownership(s);
 
-    let x = 5;
+    // let x = 5;
 
-    makes_copy(x);
+    // makes_copy(x);
 
-    //println!("{s}");
-    println!("{x}");
+    // //println!("{s}");
+    // println!("{x}");
 
-    //references
-    let s1 = String::from("hello world");
+    // //references
+    // let s1 = String::from("hello world");
 
-    let len = calculate_len(&s1);
+    // let len = calculate_len(&s1);
 
-    println!("The length of {s1} is {len}.");
+    // println!("The length of {s1} is {len}.");
+
+    //mutable references
+
+    let mut s_mut = String::from("Hello");
+
+    s_mut.push_str(", world!");
+
+    println!("{s_mut}");
+
+    change(&mut s_mut);
+    println!("{}", s_mut);
 }
 
-fn take_ownership(some_string: String) {
-    println!("{some_string}");
+// fn take_ownership(some_string: String) {
+//     println!("{some_string}");
 
-}
+// }
 
-fn makes_copy(some_integer: i32){
-    println!("{some_integer}");
-}
+// fn makes_copy(some_integer: i32){
+//     println!("{some_integer}");
+// }
 
-fn calculate_len(s2: &String) -> usize {
-    s2.len()
+// fn calculate_len(s2: &String) -> usize {
+//     s2.len()
+// }
+fn change(some_string: &mut String) {
+    some_string.push_str(", Welcome to the new world of Rust!!");
 }
